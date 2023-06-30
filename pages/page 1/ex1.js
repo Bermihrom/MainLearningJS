8d25aa8b2a0a0d044cbd2abe1f2b668442b2628b
Bermihrom <funnyhak876@gmail.com>
2023-06-30 18:57:59 +0300

    доделал сайд-бар, сделал первое задание

---
+++ pages/page 1/ex1.js
@@ -0,0 +1,28 @@
+// №1
+console.log("Misha");
+// №2
+console.log("04");
+// №3
+console.log('Добро '+'пожаловать '+' на курс');
+// №4
+// alert("2019");
+// №5
+// alert(200 - 19);
+// №6
+let a = document.getElementById("one");
+a.innerHTML = "Hello world!!";
+// №7
+let b = document.getElementById("two");
+b.innerHTML = 12 * 12;
+// №8
+let c = document.querySelector(".three");
+c.innerHTML = "Hello world!!!!";
+// №9
+let d = document.querySelector("span");
+d.innerHTML = "world";
+// №10
+let e = document.querySelector(".four");
+let txt = document.createElement("h3");
+txt.innerHTML = "dvgfuhdevgfniiswivi"
+e.append(txt);
+// №11
